586a7cc348b6dbb52eee5d8593740a5fa795a888
Daniel Hsieh <danielcdcj@yahoo.com.tw>
2015-10-24 16:55:00 -0800

    Avatar Class

--- www/class/Avatar.js
+++ www/class/Avatar.js
@@ -1,9 +1,47 @@
 function Avatar(name){
-	// name (String): name of the startup
-	this.name = name;
+	var name = name;
+	var rage = null;
+	var cash = null;
+	var listOfInvestments = [];
 	
-	this.cash = 0;
-	this.listOfInvestment = [];
 	
+	// Name
+	this.getName = function(){
+		return name;
+	}
+	this.setName = function(newName){
+		name = newName;
+	}
 	
+	// Rage
+	this.getRage = function(){
+		return rage;
+	}
+	this.setRage = function(newRageValue){
+		rage = newRageValue;
+	}
+	
+	// Cash
+	this.getCashAmount = function(){
+		return cash;
+	}
+	this.setCashAmount = function(newCashAmount){
+		cash = newCashAmount;
+	}
+	
+	// Investment
+	this.getListOfInvestment = function(){
+		return listOfInvestments;
+	}
+	
+	// Object IO
+	this.toString = function(){
+		var theObject = {
+			'name':name,
+			'cash':cash,
+			'investments':listOfInvestments
+		}
+		
+		return JSON.stringify(theObject);
+	}
 }
