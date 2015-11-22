8b253430be467e38dc5abec82c6b01a9f4427bfd
Daniel Hsieh <danielcdcj@yahoo.com.tw>
2015-11-22 13:36:53 -0800

    profile pucture

--- www/js/UIControl/index.js
+++ www/js/UIControl/index.js
@@ -1,7 +1,24 @@
 function refreshStatusBar(avatarInstance){
 	avatarInstance.visitInvestments();
 	$('#avatarName_display').html(avatarInstance.getName());
-	$('#avatarRage_display').html(avatarInstance.getRage());
-	$('#avatarNetWorth_display').html(avatarInstance.getNetWorth().toFixed(2));
 	$('#avatarCash_display').html(avatarInstance.getCashAmount().toFixed(2));
+	
+	//modal
+	$('#avatarName_modal').html(avatarInstance.getName());
+	$('#avatarNetworth_modal').html(avatarInstance.getNetWorth().toFixed(2));
+	
+	
+}
+function initialLoad(avatarInstance){
+	// load avatar image
+	if(!localStorage.getItem('profilePicture')) { //if no image saved
+		console.log('there had been no picture saved');
+		localStorage.setItem("profilePicture", 'img/profile_empty.jpg');
+	} else {
+		console.log('there had been picture saved');
+	}
+	
+	var imageData = localStorage.getItem('profilePicture');
+	document.getElementById('avatarPicture').src = imageData;
+	document.getElementById('avatarPicture_modal').src = imageData;
 }
